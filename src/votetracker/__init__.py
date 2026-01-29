99810241604cb0614bb50515e44b4649dcbd4188
Mambuco <gabriele.giambrone@icloud.com>
2026-01-29 19:38:06 +0100

    chore: Bump version to 2.8.0

--- src/votetracker/__init__.py
+++ src/votetracker/__init__.py
@@ -3,5 +3,5 @@ VoteTracker - School Grade Management Application
 Track grades, calculate averages, and simulate report cards.
 """
 
-__version__ = "2.7.1"
+__version__ = "2.8.0"
 __author__ = "VoteTracker"
